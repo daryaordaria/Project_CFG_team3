d20cde974de6d216af168cc43d3285c36c6e1e06
Ula Kaminska <ursz.kam@gmail.com>
2022-11-01 13:26:41 +0100

    .

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, flash, render_template, request, url_for
 from .connect_database import get_adverts_by_id, get_adverts_by_location
-from .functions import extract_data
+from .lat_long import extract_lat_long_via_address, calculate_distance
+# from .functions import extract_data
 
 app = Flask(__name__, template_folder='templates', static_url_path='/static')
 app.secret_key = 'I#love<3cookies'
@@ -25,12 +26,13 @@ def search():
 @app.route('/results', methods=['GET','POST'])
 def results():
     address = request.form.get('address').strip()
-    city, country = extract_data(address)
-    results = get_adverts_by_location(city, country)
+    coords = extract_lat_long_via_address(address)
+    area = calculate_distance(coords)
+    results = get_adverts_by_location(coords)
     
     return render_template(
         'results.html',
-        results
+        results=results
         )
 
 
@@ -39,7 +41,7 @@ def item_details(id):
     result = get_adverts_by_id(id)
     return render_template(
         'item.html',
-        result)
+        result=result)
 
 
 @app.route('/login', methods=['GET','POST'])
--- connect_database.py
+++ connect_database.py
@@ -27,18 +27,19 @@ def _get_adverts(query):
     cur.execute(query)
     results = cur.fetchall()  # this is a list with db records where each record is a tuple
 
-    keys = ['userID', 'annoucementID', 'country', 'city', 'pick_up_details', 
-    'expiration_date', 'vegan', 'vegetarian', 'kosher', 'halal', 'glutenfree', 
-    'lactosefree', 'product_name', 'description', 'status']
+    keys = ['unnoucementID', 'address', 'latitude', 'longitude' 'pick_up_details', 
+            'expiration_date', 'vegan', 'vegetarian', 'kosher', 'halal', 'glutenfree', 
+            'lactosefree', 'product_name', 'description']
     tags = ['vegan', 'vegetarian', 'kosher', 'halal', 'glutenfree', 'lactosefree']
     
     for result in results:
         result_dict = dict(zip(keys, result))
         
         tags = [key for key, val in result_dict.items() if key in tags and val == 1]
-        result_dict[tags] = ", ".join(tags)
+        result_dict["tags"] = ", ".join(tags)
         
         adverts.append(result_dict) 
+        
     cur.close()
     
     if db_connection:
@@ -47,13 +48,17 @@ def _get_adverts(query):
     return adverts
 
 
-def get_adverts_by_location(city, country):
+def get_adverts_by_location(coords):
     query = f"""
-        SELECT userID, annoucmentID, country, city, pick_up_details, 
-        expiration_date, vegan, vegetarian, kosher, halal, glutenfree, 
-        lactosefree, product_name, description, status
-        FROM annoucements 
-        WHERE city = '{city}' AND country = '{country}' 
+        SELECT 
+            unnoucementID, address, latitude, longitude, pick_up_details, 
+            expiration_date, vegan, vegetarian, kosher, halal, glutenfree, 
+            lactosefree, product_name, description,
+        FROM 
+            annoucements 
+        WHERE 
+            latitude BETWEEN '{coords["lat_min"]}' AND '{coords["lat_max"]}'
+            AND longitude BETWEEN '{coords["lon_min"]}' AND '{coords["lon_max"]}' 
         """
 
     result = _get_adverts(query)
@@ -62,11 +67,14 @@ def get_adverts_by_location(city, country):
 
 def get_adverts_by_id(id):
     query = f"""
-        SELECT userID, annoucmentID, country, city, pick_up_details, 
-        expiration_date, vegan, vegetarian, kosher, halal, glutenfree, 
-        lactosefree, product_name, description, status
-        FROM annoucements 
-        WHERE annoucmentID = '{id}'
+        SELECT 
+            annoucmentID, address, latitude, longitude, pick_up_details, 
+            expiration_date, vegan, vegetarian, kosher, halal, glutenfree, 
+            lactosefree, product_name, description
+        FROM 
+            annoucements 
+        WHERE 
+            annoucmentID = '{id}'
         """
     result = _get_adverts(query)
     return result
