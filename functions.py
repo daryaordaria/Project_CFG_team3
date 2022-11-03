b9a096e9e3c84697a8dc94e78c82b03df6be2ff3
Ula Kaminska <ursz.kam@gmail.com>
2022-11-03 01:52:24 +0100

    .

--- functions.py
+++
@@ -1,8 +0,0 @@
-# address = "Warsaw, Poland"
-import re
-
-def extract_data(address):
-    address_list = address.split(",")
-    city = address_list[0].strip()
-    country = address_list[1].strip()
-    return city, country
