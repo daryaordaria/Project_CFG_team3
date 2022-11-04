51189305d3ecb64dbdc1fcd6f262fcdcc7a448f5
Ula Kaminska <ursz.kam@gmail.com>
2022-11-04 01:34:22 +0100

    refactor: added namedtuple

--- lat_long.py
+++ lat_long.py
@@ -1,24 +1,27 @@
 from haversine import inverse_haversine, Direction
+from collections import namedtuple
 import requests
 
 GOOGLE_API_KEY = 'AIzaSyApyx4KbXuO-hBRceBu3LugFm-rYdAlvPQ'
 
 def extract_lat_long_via_address(address):
-    lat, lng = None, None
+    Coords = namedtuple('Coords',('lat', 'lon'))
+    coords = Coords(None, None)
+    
     api_key = GOOGLE_API_KEY
     base_url = "https://maps.googleapis.com/maps/api/geocode/json"
     endpoint = f"{base_url}?address={address}&key={api_key}"
     r = requests.get(endpoint)
     if r.status_code not in range(200, 299):
-        return None, None
+        return coords
     try:
         results = r.json()['results'][0]
-        lat = results['geometry']['location']['lat']
-        lng = results['geometry']['location']['lng']
+        coords.lat = results['geometry']['location']['lat']
+        coords.lon = results['geometry']['location']['lng']
     except:
         pass
     
-    return lat, lng
+    return coords
 
 
 def calculate_distance(coords):
