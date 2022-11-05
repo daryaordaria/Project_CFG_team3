fb5b3d8e77f010107187233c55afc21d4f06bffa
Ula Kaminska <ursz.kam@gmail.com>
2022-11-05 23:28:02 +0100

    added google api key

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 HOST="localhost"
 USER="root"
-PASSWORD=""
+PASSWORD="Karolina12"
 DB_NAME="Sherfood"
+
 EMAIL_USER = "shefood.project3@gmail.com"
 EMAIL_PASSWORD ="cehysmssaqsnjzwu"
+
+GOOGLE_API_KEY = 'AIzaSyApyx4KbXuO-hBRceBu3LugFm-rYdAlvPQ'
